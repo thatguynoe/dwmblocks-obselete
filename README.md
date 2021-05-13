b71aa1f6e0bd1c829da2e018ab228a568ccf86bf
thatguynoe <noereyesrivas@gmail.com>
2021-05-12 22:08:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Noe's build of dwmblocks
+
+My build of dwmblocks is pretty much standard, except for the scripts I used, which of course, came from [Luke Smith](https://github.com/LukeSmithxyz/voidrice/tree/master/.local/bin/statusbar). I only made some small, personal modifications, such as [removing newlines from command output](https://github.com/LukeSmithxyz/dwmblocks/pull/12/commits/6bbd6545f422bab6dfd341d88e99316f4cdab7ac) and adding [bar padding](https://github.com/flipMWD/dwmblocks/commit/82efdb743e207df9c74d8123bcc2e8502ce97786).
+
+## Installation
+
+```
+git clone https://github.com/thatguynoe/dwmblocks
+cd dwmblocks
+sudo make install
+```
---
+++ config.h
@@ -0,0 +1,26 @@
+// Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+    /*Icon*/    /*Command*/     /*Update Interval*/ /*Update Signal*/
+    {"",        "cat /tmp/recordingicon 2>/dev/null",  0,  9},
+    /* {" ",       "music",        0,                  11}, */
+    /* {"",	    "pacpackages",	0,	                8}, */
+    {"",        "memory",       10,                 14},
+    {"",        "cpu",          10,                 18},
+    {"",        "disk",         10800,              6},
+    {"",        "bright",       0,                  19},
+    {"",        "moonphase",    18000,              17},
+    {"",        "weather",      18000,              5},
+    {"",        "mailbox",      180,                12},
+    {"",        "volume",       0,                  10},
+    {"",        "battery",      5,                  3},
+    {"",        "internet",     5,                  4},
+    {"",        "clock",        60,                 1},
+};
+
+// Sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 3;
+
+// Sets delimiters around the full statusbar. NULL character ('\0') means no delimeter.
+static char leftpad[]  = " ";
+static char rightpad[] = "\0";
